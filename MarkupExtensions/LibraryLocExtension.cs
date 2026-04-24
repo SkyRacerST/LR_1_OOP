83b15c49051e37e5a4a639800f916a00b5ab2554
Николай <brawlstarsofficial@gmail.com>
2026-04-25 02:45:58 +0300

    Добавлена поддержка локализации с помощью внешней библиотеки

---
+++ MarkupExtensions/LibraryLocExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+using LocalizationLibrary;
+
+namespace LR_1_Default.MarkupExtensions
+{
+    public class LibraryLocExtension : MarkupExtension
+    {
+        [ConstructorArgument("key")]
+        public string Key { get; set; }
+
+        public LibraryLocExtension() { }
+
+        public LibraryLocExtension(string key)
+        {
+            Key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(Key))
+                return string.Empty;
+
+            var service = LocalizationManager.Instance;
+
+            var binding = new Binding
+            {
+                Source = service,
+                Path = new System.Windows.PropertyPath($"Item[{Key}]"),
+                Mode = BindingMode.OneWay
+            };
+
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ ViewModels/LibraryLocalizedMainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using LocalizationLibrary;
+
+namespace LR_1_Default.ViewModels
+{
+    public class LibraryLocalizedMainWindowViewModel : INotifyPropertyChanged
+    {
+        private CultureInfo _selectedLanguage;
+
+        public DefaultBindingViewModel DefaultBindingVM { get; set; }
+        public TwoWayBindingViewModel TwoWayBindingVM { get; set; }
+        public OneTimeBindingViewModel OneTimeBindingVM { get; set; }
+        public OneWayBindingViewModel OneWayBindingVM { get; set; }
+        public TriggersViewModel TriggersVM { get; set; }
+
+        public ObservableCollection<CultureInfo> AvailableLanguages { get; set; }
+
+        public CultureInfo SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set
+            {
+                if (value != null)
+                {
+                    _selectedLanguage = value;
+                    OnPropertyChanged(nameof(SelectedLanguage));
+                    LocalizationManager.Instance.SetLanguage(value.Name);
+                }
+            }
+        }
+
+        public LibraryLocalizedMainWindowViewModel()
+        {
+            DefaultBindingVM = new DefaultBindingViewModel();
+            TwoWayBindingVM = new TwoWayBindingViewModel();
+            OneTimeBindingVM = new OneTimeBindingViewModel();
+            OneWayBindingVM = new OneWayBindingViewModel();
+            TriggersVM = new TriggersViewModel();
+
+            AvailableLanguages = new ObservableCollection<CultureInfo>
+            {
+                new CultureInfo("ru-RU"),
+                new CultureInfo("en-US")
+            };
+
+            _selectedLanguage = AvailableLanguages.FirstOrDefault(c => c.Name == "ru-RU");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
